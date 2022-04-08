875bd39d35e1d21e3f3e8851e97f4f3ae21b47a4
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-04-07 21:15:05 -0500

    Poking the bear
    Trying out a function to possibly separate Ace 11 from Ace 1?  Not sure yet.  Seems like a good idea anyway.

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
 import random
-# So the first thing I'm going to
-# need is a while loop to actually make
-# the game loop around.  I'm going to do that
-# first because otherwise the damn thing breaks.
+# Based on Ace being worth 11 until the hand > 21 when it becomes 1
+# I'm assuming 11 as value for now.  I'm going to try putting
+# cards in a function so I can *hopefully*
+# call a later function with 1 instead of 11.  Fingers crossed :)
+def dealCard(): # Note to self. () are important dum dum!
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-# Second thing I'll need to do is set up a dictionary
-# or a list to define the cards.  Initially I'm just going to
-# be working with straight numbers, i.e. 1 = Ace, 11 = Jack, etc
-#I can figure out the rest once I've got the bare bones basics working.
-#When I do get that working though I'm going to need four suits of cards
-# I think a Dictionary and then a List will be the way to go here.
-# Hearts: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11: Jack, 12: Queen, 13: King, maybe?]
-# No idea how I'm going to handle the logic of Ace being worth 1 or 11 though...
-
-# Game AI will be easy enough, just have a random choice
-# pull from the list, or heck, in this early stage, a simple
-# call from a range.
-
-# If Dealer hand > 18 and Player hand <= 18, Stand.
-# That way the Dealer still tries to win if the player
-# is winning.
