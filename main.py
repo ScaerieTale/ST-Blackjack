c4f8e7b8da780a05e8da45d63cc095219f421b7e
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-04-07 21:21:27 -0500

    Testing Function dealCard
    And wouldn't you know it, I got 21, AFTER I already got a 2., and busted.  Story of my life hey? ;)  Anyway function works!  YAY!

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ import random
 # cards in a function so I can *hopefully*
 # call a later function with 1 instead of 11.  Fingers crossed :)
 def dealCard(): # Note to self. () are important dum dum!
+    """Deal a random card from Ace (11) to King (one of four 10s)"""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
+    dealtCard = random.choice(cards)
+    return dealtCard
+print(dealCard())
