e4def1d3d1de2014a15d2c9dbdbd4aceb9dfada7
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-04-07 21:57:32 -0500

    Code update + screenshot for lols
    See the readme for why the screenshot's been added (hint: look in the bottom Terminal).  Dealer/Player hand initial hand is working though!

--- main.py
+++ main.py
@@ -8,4 +8,23 @@ def dealCard(): # Note to self. () are important dum dum!
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     dealtCard = random.choice(cards)
     return dealtCard
-print(dealCard())
+
+#I'm looking at the sample game again and I notice she uses
+# a list to keep track of the cards.  That one had me scratching
+# my head at first wondering how I'd avoid
+# the cards getting added together in the display haha
+
+#Going to just make two empty lists for now that I can write to.  Probably need
+# an iterating loop (for loop?) to deal 2 cards each.
+
+dealerHand = []
+playerHand = []
+for i in range(2):
+        dealerHand.append(dealCard())
+        playerHand.append(dealCard())
+
+
+# Dealer hand shows index 0 only because player only gets
+# to see Dealer's first card until the hand ends.
+print(f"""Dealer shows {dealerHand[0]}
+You have {playerHand}""")
